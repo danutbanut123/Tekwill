41c406b2d1657bc36aca1edf5a79e58bf1bd3128
Dan Selevestru <lolnormcool@gmail.com>
2020-06-29 20:58:01 +0300

    second commit

---
+++ lesson 3/excersise 3.1/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+      byte b=10;
+      short s=b;
+       int i=s;
+        long l=i;
+       float f=l;
+       double d=f;
+
+     float ff=(float)d;
+     long ll= (long) ff;
+     int ii=(int)ll;
+     short ss=(short)ii;
+     byte bb =(byte)ss;
+System.out.println(bb);
+
+
+
+
+
+    }
+
+}

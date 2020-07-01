afcc5592f952c4d659c41737d863bebaaac98f5c
Dan Selevestru <lolnormcool@gmail.com>
2020-07-01 20:52:23 +0300

    4th commit

---
+++ lesson 4/excersise 4.3/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write the value of x!");
+        double x = sc.nextDouble();
+
+
+
+        System.out.println(x*x*x+x*x+x+1);
+
+    }
+}

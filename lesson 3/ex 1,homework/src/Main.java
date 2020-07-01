fb3e7c109f3dc13649f890e4203c1e39bbb7c39d
Dan Selevestru <lolnormcool@gmail.com>
2020-07-01 16:29:03 +0300

    3rd commit

---
+++ lesson 3/ex 1,homework/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a=3;
+        int b=4;
+        int c=5;
+        System.out.println(a + " " + b + " " + c);
+    }
+}
---
+++ lesson 3/ex 3,homework/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+       int a=123456;
+        System.out.println(a);
+    }
+}

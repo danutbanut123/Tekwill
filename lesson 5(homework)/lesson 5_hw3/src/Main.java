1a802e896a9c1c65ccbb4de8156a986e02ae2689
Dan Selevestru <lolnormcool@gmail.com>
2020-07-08 14:39:56 +0300

    added hw exs from last lesson

--- lesson 5(homework)/lesson 5_hw3/src/Main.java
+++ lesson 5(homework)/lesson 5_hw3/src/Main.java
@@ -1,18 +1,20 @@
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("What is the length of the chocolate bar?");
-        int N = sc.nextInt();
+        int n = sc.nextInt();
         System.out.println("What is the width of the chocolate bar?");
-        int M = sc.nextInt();
-        int K = sc.nextInt();
-        if (K<=(N*M) && (N*M) %K ==0){
-            System.out.println("Yes");
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        if (k <= n * m && (k % n == 0 && k >= n || k % m == 0 && k >= m)) {
+            System.out.println("YES");
         }
         else {
-            System.out.println("No");
+            System.out.println("NO");
         }
     }
 }

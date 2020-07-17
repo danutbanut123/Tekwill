1a4fdcca3b9977d8966a1170fa432fa8c975ae37
Dan Selevestru <lolnormcool@gmail.com>
2020-07-17 16:42:27 +0300

    homework for lesson 8

---
+++ lesson 8(homework)/lesson 8_hw1/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static int extractInt(double d) {
+     int a =(int) d;
+     return a;
+    }
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double d = scanner.nextDouble();
+        System.out.println(extractInt(d));
+    }
+}

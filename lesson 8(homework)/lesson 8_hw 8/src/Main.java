f591dc22f5c0285222a9c8fe79e754054c9d6890
Dan Selevestru <lolnormcool@gmail.com>
2020-07-20 15:40:27 +0300

    ex 7 and 8

---
+++ lesson 8(homework)/lesson 8_hw 8/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Main {
+    public static long [] addValueByIndex(long [] array ,int index ,long value){
+      array[index]+=value;
+      return array;
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long[] array = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToLong(Long::parseLong)
+                .toArray();
+        int index = scanner.nextInt();
+        long value = scanner.nextLong();
+        addValueByIndex(array, index, value);
+        Arrays.stream(array).forEach(e -> System.out.print(e + " "));
+    }
+}
